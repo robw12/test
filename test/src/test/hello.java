a7ba6e1163662738493113913e26522223086b0b
U502920 <U502920@WU17045391-LSY.ads.dlh.de>
2017-03-30 16:50:49 +0200

    199

--- test/src/test/hello.java
+++ test/src/test/hello.java
@@ -1,9 +1,5 @@
 package test;
 
 public class hello {
-<<<<<<< HEAD
-String  a = "patch177master";
-=======
-String  a = "patch166master";
->>>>>>> refs/heads/branch2
+String  a = "patch199master";
 }
